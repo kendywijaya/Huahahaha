557fcb71ccb186e7c26cad7fb43801f248c01fc7
kendywijaya <76420698+kendywijaya@users.noreply.github.com>
2021-01-24 13:50:24 +0700

    Buat Todo

--- src/App.js
+++ src/App.js
@@ -1,49 +1,6 @@
+import TodoList from "./pages/TodoList";
 import "./styles.css";
 
 export default function App() {
-  return (
-    <div className="container">
-      <div className="frame">
-        <section className="header">
-          <button className="header-btn --main-black-color">Add</button>
-          <h1 className="header-title">Todo Lists</h1>
-          <button className="header-btn --main-red-color">Clear</button>
-        </section>
-
-        <section className="add">
-          <form className="add-form">
-            <input type="text" className="add-input"></input>
-            <button className="add-btn --main-black-color">Add</button>
-          </form>
-        </section>
-
-        <section className="todos">
-          <div className="todo">
-            <span className="todo-text">Learning React</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">Learning React</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">Learning React</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">Learning React</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">Learning React</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">Learning React</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">Learning React</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">Learning React</span>
-          </div>
-        </section>
-      </div>
-    </div>
-  );
+  return <TodoList />;
 }
